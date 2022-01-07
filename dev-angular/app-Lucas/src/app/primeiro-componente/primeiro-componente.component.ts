852c542e1f7eb5fc505c05f7c33284b5e8dc6c53
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2022-01-07 13:52:45 -0300

    Trabalhando com funcao, variavel, IF e For

--- dev-angular/app-Lucas/src/app/primeiro-componente/primeiro-componente.component.ts
+++ dev-angular/app-Lucas/src/app/primeiro-componente/primeiro-componente.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrimeiroComponenteComponent implements OnInit {
 
+  meuTitulo = "Exibindo dados, atravez de uma variavel";
+
+  texto1 = "Olá, eu sou o texto 1";
+  texto2 = "Olá, eu sou o texto 2";
+
+  cliente = {
+    id: 1,
+    nome: "Lucas"
+  }
+
+  clientes = [
+    {
+      id: 1,
+      nome: "Lucas",
+      ativo : false
+    },
+    {
+      id: 2,
+      nome: "Rhaylene",
+      ativo : true
+    },
+    {
+      id: 3,
+      nome: "Livia",
+      ativo : true
+    }
+    ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+geraTexto(){
+  return "Teste de retorno de funcao";
+}
 
 }
