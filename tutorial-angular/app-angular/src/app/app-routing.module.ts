135fa73e99d503f6cca840aa5d1d16cec3ed28f2
Lucas dos Santos Amorim Rêgo <lucass235@gmail.com>
2022-01-07 15:07:58 -0300

    trabalhando com rotas

---
+++ tutorial-angular/app-angular/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Pagina01Component } from './pagina01/pagina01.component';
+import { Pagina02Component } from './pagina02/pagina02.component';
+import { UsersComponent } from './users/users.component';
+const routes: Routes = [
+  { path: "pagina01", component: Pagina01Component },
+  { path: "pagina02",  component: Pagina02Component },
+  { path: "users",  component: UsersComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ tutorial-angular/app-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Pagina01Component } from './pagina01/pagina01.component';
+import { Pagina02Component } from './pagina02/pagina02.component';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Pagina01Component,
+    Pagina02Component,
+    UsersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
